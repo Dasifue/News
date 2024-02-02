aed676626fe2c149068912cb24f74eaa553863e3
dasifue <dasifue@gmail.com>
2024-02-02 19:43:57 +0600

    creation api

--- api/views.py
+++ api/views.py
@@ -6,12 +6,12 @@ from news.models import Article
 
 
 
-class ArticleAPIView(generics.ListAPIView):
+class ArticleAPIView(generics.ListCreateAPIView): 
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
 
-class RetrieveArticleAPIView(generics.RetrieveAPIView):
+class RetrieveArticleAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     lookup_field = "pk"
